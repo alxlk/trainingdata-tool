d91422e5cd34efa8c303f45471485faa8645141c
alxlk <39247544+alxlk@users.noreply.github.com>
2019-05-08 03:04:23 +0300

    Update trainingdata-tool.cpp

--- src/trainingdata-tool.cpp
+++ src/trainingdata-tool.cpp
@@ -41,8 +41,8 @@ uint64_t resever_bits_in_bytes(uint64_t v) {
 
 bool extract_fishtest_comment_score(const char* comment, float& Q) {
   std::string s(comment);
-  static std::regex rgx("{(-?\\d+\\.\\d+)/");
-  static std::regex rgx2("{#(-?\\d+)/");
+  static std::regex rgx("(-?\\d+\\.\\d+)/");
+  static std::regex rgx2("#(-?\\d+)/");
   std::smatch matches;
   if (std::regex_search(s, matches, rgx)) {
     Q = std::stof(matches[1].str());
